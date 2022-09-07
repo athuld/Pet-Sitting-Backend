b0f9affe754618f12fdd5d60750d0d798e1614a0
Athul Dinesan <athuldinesan5@gmail.com>
2022-09-07 19:11:14 +0530

    backend: fix CORS error
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- app/application.go
+++ app/application.go
@@ -1,12 +1,28 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
 
-var(
-    router = gin.Default()
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	router = gin.Default()
 )
 
 func StartApplication() {
-    mapUrls()
-    router.Run(":8081")
+	mapUrls()
+	router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET","POST","PATCH"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:5173"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	router.Run(":8081")
 }
